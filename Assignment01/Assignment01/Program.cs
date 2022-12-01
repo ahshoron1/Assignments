313b312865fa2c1f5a7171c5d25026a6cf6b1164
AH Shoron <anamulhaqueshoron@gmail.com>
2022-12-01 15:30:19 +0600

    changed coded

--- Assignment01/Assignment01/Program.cs
+++ Assignment01/Assignment01/Program.cs
@@ -10,12 +10,13 @@ namespace Assignment01
         static void Main(string[] args)
         {
             //===============Insert Data Student Table==============//
+
             //using (var context = new TrainingContext())
             //{
             //    var student = new Students()
             //    {
-            //        Name = "Abu Zafor bin Habib",
-            //        DateOfBirth = new DateTime(2000, 3, 3),
+            //        Name = "Abdur Rahman",
+            //        DateOfBirth = new DateTime(2005, 3, 3),
             //        Address = "Bangladesh"
             //    };
             //    context.Students.Add(student);
@@ -23,9 +24,10 @@ namespace Assignment01
             //}
 
             //==============Update Data Student Table===============//
+
             //using (var context = new TrainingContext())
             //{
-            //    var student = context.Students.Where(x => x.Id == 2).FirstOrDefault();
+            //    var student = context.Students.Where(x => x.Id == 3).FirstOrDefault();
             //    student.Name = "Abu Tha Muhammad";
             //    student.DateOfBirth = new DateTime(1987, 3, 5);
             //    student.Address = "Madina";
@@ -33,38 +35,42 @@ namespace Assignment01
 
             //}
 
-            ////==============Delete Data Student Table==================//
-            //using(var context = new TrainingContext())
+            // ==============Delete Data Student Table================== //
+
+            //using (var context = new TrainingContext())
             //{
             //    var student = context.Students.Where(x => x.Id == 2).FirstOrDefault();
             //    context.Students.Remove(student);
             //    context.SaveChanges();
             //}
 
-            ///===============Insert Data Courses Table==================//
+            ///===============Insert Data Courses Table================== // 
+
             //using (var context = new TrainingContext())
             //{
             //    var course = new Courses()
             //    {
-            //        Title = "c#",
-            //        Fees = 8000,
-            //        DurationInHours = 148
+            //        Title = "Compititive Programming",
+            //        Fees = 6000,
+            //        DurationInHours = 48
             //    };
             //    context.Courses.Add(course);
             //    context.SaveChanges();
             //}
 
             // ============== Update Data Course Table ================ //
+
             //using (var context = new TrainingContext())
             //{
             //    var course = context.Courses.Where(x => x.Id == 3).FirstOrDefault();
             //    course.Title = "Competitive Programming - Intermediate";
-            //    course.Fees = 6000;
+            //    course.Fees = 9000;
             //    course.DurationInHours = 50;
             //    context.SaveChanges();
             //}
 
             // =============== Delete Data Course Table ================ //
+
             //using (var context = new TrainingContext())
             //{
             //    var course = context.Courses.Where(x => x.Id == 3).FirstOrDefault();
@@ -72,15 +78,16 @@ namespace Assignment01
             //    context.SaveChanges();
             //}
 
-            // =============== Course && Student Table Relaton in Enrollments Table ============== //
-            using (var context = new TrainingContext())
-            {
-                var course = context.Courses.Where(x => x.Id == 1).FirstOrDefault();
-                var student = context.Students.Where(x => x.Id == 1).FirstOrDefault();
-                course.EnrollmentCourses = new List<Enrollments>();
-                course.EnrollmentCourses.Add(new Enrollments { Student = student, EnrollDate = DateTime.Now });
-                context.SaveChanges();
-            }
+            // =============== Course && Student Table Relaton in Enrollments Table ============== // 
+
+            //using (var context = new TrainingContext())
+            //{
+            //    var course = context.Courses.Where(x => x.Id == 1).FirstOrDefault();
+            //    var student = context.Students.Where(x => x.Id == 2).FirstOrDefault();
+            //    course.EnrollmentCourses = new List<Enrollments>();
+            //    course.EnrollmentCourses.Add(new Enrollments { Student = student, EnrollDate = DateTime.Now });
+            //    context.SaveChanges();
+            //}
 
 
         }
